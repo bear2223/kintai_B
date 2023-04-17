f1cca2b12c89df4c7668be33532db84ef523db1c
bear223 <gakusei_00@yahoo.co.jp>
2023-04-17 02:07:10 +0000

    8章　途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
- def create
-  @user = User.new(user_params)
-  if @user.save
-    log_in @user # 保存成功後、ログインします。
-    flash[:success] = '新規作成に成功しました。'
-    redirect_to @user
-  else
-    render :new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
   end
- end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 
   private
 
