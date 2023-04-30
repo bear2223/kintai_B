6ad484f9b82fb6f28fa516054880c5bde84c4be8
bear223 <gakusei_00@yahoo.co.jp>
2023-04-30 17:45:07 +0000

    10章途中

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20230426063023) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2023-04-21 23:00:00"
-    t.datetime "work_time", default: "2023-04-21 22:30:00"
+    t.datetime "basic_time", default: "2023-04-30 23:00:00"
+    t.datetime "work_time", default: "2023-04-30 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
